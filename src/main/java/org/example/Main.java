06be7271aa4d686a81ecfd8bcbe9fa015e46b871
Torok Roland <torok.roland1998@gmail.com>
2023-05-07 15:28:56 +0300

    insert values into table

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,7 +17,27 @@ public class Main {
                 .addAnnotatedClass(Truck.class)
                 .addAnnotatedClass(Driver.class)
                 .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
 
+        Car c = new Car();
+        c.setId(11);
+        c.setName("Test car");
+
+        session.persist(c); // un fel de insert
+
+        // driver
+        Driver driver = new Driver();
+        driver.setId(77);
+        driver.setName("Mikey");
+        driver.setEmail("mikey@gmail.com");
+        driver.setMaxTravelDistance(1000);
+        driver.setSecurityKey("Mouse");
+
+        session.persist(driver);// un fel de insert
+
+        transaction.commit(); // toate modificarile se duc in baza de date
+        session.close();
 
     }
 
