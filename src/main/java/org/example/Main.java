6d38f538e3c4b1c02b70b8b582d7e35a8623554e
Torok Roland <torok.roland1998@gmail.com>
2023-05-07 12:11:20 +0300

    add data base config

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.config.xml")
+                .buildSessionFactory();
+
+    }
+
+}
